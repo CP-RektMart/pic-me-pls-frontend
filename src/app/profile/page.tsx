94b81b2daedb5fb74c01f6d44fec98d4bdb435b0
Phumsirii <phumsirisumativit@gmail.com>
2025-02-04 22:32:24 +0700

    add: useState on Facebook

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,6 +12,7 @@ export default function ProfilePage() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
+  const [facebook, setFacebook] = useState('')
   return (
     <div className='mx-12 my-6 flex-col gap-6 space-y-6 lg:px-8'>
       <div className='flex h-10 flex-row'>
@@ -67,6 +68,7 @@ export default function ProfilePage() {
             <Input
               placeholder='Facebook'
               className='h-10 justify-between px-3 py-2'
+              onChange={(e) => setFacebook(e.target.value)}
             />
           </div>
           <div className='space-y-1.5'>
