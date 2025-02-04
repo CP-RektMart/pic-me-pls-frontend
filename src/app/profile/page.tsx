b2f56a04afb104b4ca6d8ad3fb6f671882e3adf9
Phumsirii <phumsirisumativit@gmail.com>
2025-02-04 21:22:26 +0700

    edit gap

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -29,23 +29,23 @@ export default function ProfilePage() {
           </div>
         </div>
         <div className='flex-1 space-y-8'>
-          <div className='space-y-1'>
+          <div className='space-y-1.5'>
             <p className='text-2xl'>Name</p>
             <Input placeholder='John Doe' className='px-3 py-2' />
           </div>
-          <div className='space-y-1'>
+          <div className='space-y-1.5'>
             <p className='text-2xl'>Email</p>
             <Input placeholder='admin@picmepls.com' className='px-3 py-2' />
           </div>
-          <div className='space-y-1'>
+          <div className='space-y-1.5'>
             <p className='text-2xl'>Phone</p>
             <Input placeholder='0xx-xxx-xxxx' className='px-3 py-2' />
           </div>
-          <div className='space-y-1'>
+          <div className='space-y-1.5'>
             <p className='text-2xl'>Facebook</p>
             <Input placeholder='Facebook' className='px-3 py-2' />
           </div>
-          <div className='space-y-1'>
+          <div className='space-y-1.5'>
             <p className='text-2xl'>Instagram</p>
             <Input placeholder='instagram' className='px-3 py-2' />
           </div>
@@ -55,15 +55,15 @@ export default function ProfilePage() {
               <h2 className='items-center self-center text-[24px] font-bold'>
                 Payment Method
               </h2>
-              <div className='space-y-1'>
+              <div className='space-y-1.5'>
                 <p className='text-2xl'>Bank</p>
                 <Input placeholder='SCB' className='px-3 py-2' />
               </div>
-              <div className='space-y-1'>
+              <div className='space-y-1.5'>
                 <p className='text-2xl'>Account No.</p>
                 <Input placeholder='360-411175-6' className='px-3 py-2' />
               </div>
-              <div className='space-y-1'>
+              <div className='space-y-1.5'>
                 <p className='text-2xl'>Branch</p>
                 <Input
                   placeholder='Future Park Rangsit'
