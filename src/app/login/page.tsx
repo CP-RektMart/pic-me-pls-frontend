2a490bef34e98fc7bc2f8048b5b86c871f6aa6cb
Phumsirii <phumsirisumativit@gmail.com>
2025-02-04 12:40:40 +0700

    initialise login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="h-[297.75px] w-[361px] justify-center flex flex-col items-center space-y-4">
+                <img src="logo.svg"/>
+                <h1 className="text-base font-medium">Sign in as</h1>
+                <p className="text-xs text-zinc-500">Sign in to book photographers or showcase your talent.</p>
+            </div>
+        </div>
+    )
+}
