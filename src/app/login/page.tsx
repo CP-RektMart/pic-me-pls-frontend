30b3acc31f7b3f457b58d5275a40dce85922ffc6
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2025-02-05 14:48:56 +0700

    feat: add shadcn drawer & minor change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,13 +2,27 @@
 
 import { useState } from 'react'
 
+import { Icon } from '@iconify/react'
 import Image from 'next/image'
+import { useMediaQuery } from 'react-responsive'
 
 import SignIn from '@/components/sign-in'
+import { Button } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 
 export default function LoginPage() {
   const [userType, setUserType] = useState('Customer')
-  const [isSignIn, setIsSignIn] = useState(false)
+  const [isSignIn, setIsSignIn] = useState(true)
+  const isMobile = useMediaQuery({ maxWidth: 767 })
 
   return (
     <div className='flex h-screen w-full'>
@@ -20,18 +34,98 @@ export default function LoginPage() {
             <p className='text-xs text-zinc-500'>
               Sign in to book photographers or showcase your talent.
             </p>
-            <div className='flex flex-row space-x-4'>
-              <Image src='customerIcon.svg' alt={''} width={152} height={154} />
-              <Image
-                src='photographerIcon.svg'
-                alt={''}
-                width={152}
-                height={154}
-              />
-            </div>
+            {!isMobile ? (
+              <div className='flex flex-row space-x-4'>
+                <Image
+                  src='customerIcon.svg'
+                  alt={''}
+                  width={152}
+                  height={154}
+                />
+                <Image
+                  src='photographerIcon.svg'
+                  alt={''}
+                  width={152}
+                  height={154}
+                />
+              </div>
+            ) : (
+              <div className='flex flex-row items-center space-x-4'>
+                <Drawer open>
+                  <DrawerTrigger>
+                    <Image
+                      src='customerIcon.svg'
+                      alt={''}
+                      width={152}
+                      height={154}
+                    />
+                  </DrawerTrigger>
+                  <DrawerContent className='rounded-t-3xl'>
+                    <DrawerHeader className='flex flex-col items-center'>
+                      <DrawerTitle>Sign in as Customer</DrawerTitle>
+                      <DrawerDescription>
+                        Log in to find the perfect photographer and bring your
+                        vision to life!
+                      </DrawerDescription>
+                      <Image
+                        src='CustomerSigninIcon.svg'
+                        alt=''
+                        width={152}
+                        height={154}
+                      />
+                    </DrawerHeader>
+                    <DrawerFooter className='flex items-center'>
+                      <Button type='submit' className='h-10 w-[80%]'>
+                        <Icon
+                          icon='flat-color-icons:google'
+                          width='12'
+                          height='12'
+                        />
+                        Login with email
+                      </Button>
+                    </DrawerFooter>
+                  </DrawerContent>
+                </Drawer>
+                <Drawer>
+                  <DrawerTrigger>
+                    <Image
+                      src='photographerIcon.svg'
+                      alt={''}
+                      width={152}
+                      height={154}
+                    />
+                  </DrawerTrigger>
+                  <DrawerContent className='rounded-t-3xl'>
+                    <DrawerHeader className='flex flex-col items-center'>
+                      <DrawerTitle>Sign in as Photographer</DrawerTitle>
+                      <DrawerDescription>
+                        Log in to connect with clients and grow your photography
+                        business!
+                      </DrawerDescription>
+                      <Image
+                        src='PhotographerSigninIcon.svg'
+                        alt=''
+                        width={152}
+                        height={154}
+                      />
+                    </DrawerHeader>
+                    <DrawerFooter className='flex items-center'>
+                      <Button type='submit' className='h-10 w-[80%]'>
+                        <Icon
+                          icon='flat-color-icons:google'
+                          width='12'
+                          height='12'
+                        />
+                        Login with email
+                      </Button>
+                    </DrawerFooter>
+                  </DrawerContent>
+                </Drawer>
+              </div>
+            )}
           </div>
         </div>
-      ) : (
+      ) : !isMobile ? (
         <div className='flex w-full flex-col items-center justify-center md:w-1/2'>
           <div className='flex h-[297.75px] w-[361px] flex-col items-center justify-center space-y-4'>
             <Image alt={''} src='logo.svg' width={64} height={67.75} />
@@ -54,6 +148,88 @@ export default function LoginPage() {
           ) : userType === 'Photographer' ? (
             <SignIn userType='Photographer' />
           ) : null}
+        </div>
+      ) : (
+        <div className='flex w-full flex-col items-center justify-center'>
+          <div className='flex h-[297.75px] w-[361px] flex-col items-center justify-center space-y-4'>
+            <Image alt={''} src='logo.svg' width={64} height={67.75} />
+            <h1 className='text-base font-medium'>Sign in as</h1>
+            <p className='text-xs text-zinc-500'>
+              Sign in to book photographers or showcase your talent.
+            </p>
+            <div className='flex flex-row items-center space-x-4'>
+              <Drawer>
+                <DrawerTrigger>
+                  <Image
+                    src='customerIcon.svg'
+                    alt={''}
+                    width={152}
+                    height={154}
+                  />
+                </DrawerTrigger>
+                <DrawerContent className='rounded-t-3xl'>
+                  <DrawerHeader className='flex flex-col items-center'>
+                    <DrawerTitle>Sign in as Customer</DrawerTitle>
+                    <DrawerDescription>
+                      Log in to find the perfect photographer and bring your
+                      vision to life!
+                    </DrawerDescription>
+                    <Image
+                      src='CustomerSigninIcon.svg'
+                      alt=''
+                      width={152}
+                      height={154}
+                    />
+                  </DrawerHeader>
+                  <DrawerFooter className='flex items-center'>
+                    <Button type='submit' className='h-10 w-[80%]'>
+                      <Icon
+                        icon='flat-color-icons:google'
+                        width='12'
+                        height='12'
+                      />
+                      Login with email
+                    </Button>
+                  </DrawerFooter>
+                </DrawerContent>
+              </Drawer>
+              <Drawer>
+                <DrawerTrigger>
+                  <Image
+                    src='photographerIcon.svg'
+                    alt={''}
+                    width={152}
+                    height={154}
+                  />
+                </DrawerTrigger>
+                <DrawerContent className='rounded-t-3xl'>
+                  <DrawerHeader className='flex flex-col items-center'>
+                    <DrawerTitle>Sign in as Photographer</DrawerTitle>
+                    <DrawerDescription>
+                      Log in to connect with clients and grow your photography
+                      business!
+                    </DrawerDescription>
+                    <Image
+                      src='PhotographerSigninIcon.svg'
+                      alt=''
+                      width={152}
+                      height={154}
+                    />
+                  </DrawerHeader>
+                  <DrawerFooter className='flex items-center'>
+                    <Button type='submit' className='h-10 w-[80%]'>
+                      <Icon
+                        icon='flat-color-icons:google'
+                        width='12'
+                        height='12'
+                      />
+                      Login with email
+                    </Button>
+                  </DrawerFooter>
+                </DrawerContent>
+              </Drawer>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,21 @@ import { Icon } from "@iconify/react";
 
 export default function Home() {
   return (
-    <div className='relative'>
-      <div>Pic me pls YOYO</div>
-      <div>
-        {/* <Button>Button from shadcn</Button> */}
-        {/* <Icon icon="flat-color-icons:google" width="48" height="48" /> */}
-        <SignIn userType='Customer'/>
-      </div>
-      <div className='flex flex-row space-x-2'>
-        <div className='size-10 rounded-full bg-base-primary'></div>
-        <div className='size-10 rounded-full bg-base-secondary'></div>
-        <div className='size-10 rounded-full bg-base-tertiary'></div>
-        <div className='size-10 rounded-full bg-base-quaternary'></div>
-        <div className='size-10 rounded-full bg-base-quinary'></div>
-      </div>
-    </div>
+    // <div className='relative'>
+    //   <div>Pic me pls YOYO</div>
+    //   <div>
+    //     {/* <Button>Button from shadcn</Button> */}
+    //     {/* <Icon icon="flat-color-icons:google" width="48" height="48" /> */}
+    //     <SignIn userType='Customer'/>
+    //   </div>
+    //   <div className='flex flex-row space-x-2'>
+    //     <div className='size-10 rounded-full bg-base-primary'></div>
+    //     <div className='size-10 rounded-full bg-base-secondary'></div>
+    //     <div className='size-10 rounded-full bg-base-tertiary'></div>
+    //     <div className='size-10 rounded-full bg-base-quaternary'></div>
+    //     <div className='size-10 rounded-full bg-base-quinary'></div>
+    //   </div>
+    // </div>
+    <SignIn userType='Customer'/>
   )
 }
