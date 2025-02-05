602c4783212c28f74e21ab2e09c88ff5eebf7e7a
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-02-05 20:54:52 +0700

    feat: responsive

--- src/app/photographer/verify/page.tsx
+++ src/app/photographer/verify/page.tsx
@@ -1,4 +1,5 @@
 import { CalendarIcon } from 'lucide-react'
+import Image from 'next/image'
 
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
@@ -15,55 +16,67 @@ import {
 export default function Page() {
   return (
     <div className='p-4'>
-      <p className='mb-4 font-bold'>Verify your account</p>
-      <div className='flex flex-col gap-8'>
-        <ImageUpload />
-        <div className='flex flex-col gap-2'>
-          <div className='space-y-1.5'>
-            <p className='font-medium'>Citizen ID</p>
-            <Input placeholder='1-XXXX-XXXXX-XX-X' />
-          </div>
-          <div className='flex flex-row justify-between gap-2'>
-            <div className='flex-1 space-y-1.5'>
-              <p className='font-medium'>Expired Date</p>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant='outline'
-                    className='flex w-full justify-between'
-                  >
-                    <p className='text-zinc-500'>21 Mar 2024</p>
-                    <CalendarIcon size={16} color='#71717A' />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className='w-auto p-0'>
-                  <Calendar mode='single' />
-                </PopoverContent>
-              </Popover>
-            </div>
-            <div className='flex-1 space-y-1.5'>
-              <p className='font-medium'>Laser No.</p>
-              <Input placeholder='MEx-xxxxxx-xx' />
-            </div>
+      <p className='mb-4 font-bold lg:text-2xl'>Verify your account</p>
+      <div className='flex flex-row justify-between gap-8'>
+        <div className='hidden lg:flex-1'>
+          <div className='flex h-full flex-1 items-center justify-center'>
+            <Image
+              src={'/photographer-verify.svg'}
+              alt='pic'
+              width={380}
+              height={200}
+            />
           </div>
         </div>
-        <div>
-          <div className='flex space-x-2'>
-            <Checkbox id='terms' />
-            <div className='flex flex-col space-y-1.5 leading-none'>
-              <Label
-                htmlFor='terms'
-                className='mt-0.5 text-sm font-medium leading-none'
-              >
-                Accept terms and conditions
-              </Label>
-              <p className='text-sm text-zinc-500'>
-                You agree to our Terms of Service and Privacy Policy.
-              </p>
+        <div className='flex flex-1 flex-col gap-8'>
+          <ImageUpload />
+          <div className='flex flex-col gap-2'>
+            <div className='space-y-1.5'>
+              <p className='font-medium'>Citizen ID</p>
+              <Input placeholder='1-XXXX-XXXXX-XX-X' />
+            </div>
+            <div className='flex flex-row justify-between gap-2'>
+              <div className='flex-1 space-y-1.5'>
+                <p className='font-medium'>Expired Date</p>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant='outline'
+                      className='flex w-full justify-between'
+                    >
+                      <p className='text-zinc-500'>21 Mar 2024</p>
+                      <CalendarIcon size={16} color='#71717A' />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className='w-auto p-0'>
+                    <Calendar mode='single' />
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <div className='flex-1 space-y-1.5'>
+                <p className='font-medium'>Laser No.</p>
+                <Input placeholder='MEx-xxxxxx-xx' />
+              </div>
             </div>
           </div>
+          <div>
+            <div className='flex space-x-2'>
+              <Checkbox id='terms' />
+              <div className='flex flex-col space-y-1.5 leading-none'>
+                <Label
+                  htmlFor='terms'
+                  className='mt-0.5 text-sm font-medium leading-none'
+                >
+                  Accept terms and conditions
+                </Label>
+                <p className='text-sm text-zinc-500'>
+                  You agree to our Terms of Service and Privacy Policy.
+                </p>
+              </div>
+            </div>
+          </div>
+          <Button className='self-end'>Submit</Button>
         </div>
-        <Button className='self-end'>Submit</Button>
       </div>
     </div>
   )
