2ed9e8cfc47429325c6eb94740d1f5d69e49ee9f
ChanatpakornD <csirintronsopon8@gmail.com>
2025-02-07 21:49:32 +0700

    style: format

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,20 +2,20 @@ import Container from '@/components/container'
 
 export default function NotFound() {
   return (
-    <div className='flex flex-1 items-center justify-center min-h-screen text-center'>
+    <div className='flex min-h-screen flex-1 items-center justify-center text-center'>
       <Container>
-      <div className='flex flex-col items-center justify-center space-y-4'>
-        <h1 className='animate-bounce text-3xl font-semibold'>
-        404
-        <br />
-        Page Not Found
-        </h1>
-        <p>
-        The requested URL was not found.
-        <br />
-        That’s all we know.
-        </p>
-      </div>
+        <div className='flex flex-col items-center justify-center space-y-4'>
+          <h1 className='animate-bounce text-3xl font-semibold'>
+            404
+            <br />
+            Page Not Found
+          </h1>
+          <p>
+            The requested URL was not found.
+            <br />
+            That’s all we know.
+          </p>
+        </div>
       </Container>
     </div>
   )
