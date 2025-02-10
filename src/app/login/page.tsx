d2ec7f97c489b9b84de6e86d94c2c9ca34c98de7
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2025-02-11 00:22:07 +0700

    feat: login next auth dai laew I SUS

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,9 @@
+import { auth } from '@/auth'
+
 import LoginPageComponent from '@/components/login-page'
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth()
+  console.log('Session on server:', session)
   return <LoginPageComponent />
 }
