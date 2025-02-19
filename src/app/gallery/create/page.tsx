364910debe67a60e68bbcae224429f25fff1d9ef
Phumsirii <phumsirisumativit@gmail.com>
2025-02-19 20:45:21 +0700

    feat: create gallery layout

---
+++ src/app/gallery/create/page.tsx
@@ -0,0 +1,12 @@
+import GalleryDetailSection from '@/components/gallery-page/gallery-detail'
+
+export default function CreateGalleryPage() {
+  return (
+    <div className='w-full'>
+      <div className='w-1/4'>
+        <GalleryDetailSection />
+      </div>
+      <div className='w-3/4'></div>
+    </div>
+  )
+}
