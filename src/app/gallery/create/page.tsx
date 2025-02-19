cc8829818090766737c4996593af1cafc537d373
Phumsirii <phumsirisumativit@gmail.com>
2025-02-19 21:32:23 +0700

    fix: styling

--- src/app/gallery/create/page.tsx
+++ src/app/gallery/create/page.tsx
@@ -3,7 +3,7 @@ import GalleryDetailSection from '@/components/gallery-page/gallery-detail'
 export default function CreateGalleryPage() {
   return (
     <div className='w-full'>
-      <div className='w-1/4'>
+      <div className='w-1/3'>
         {/* defaultValues */}
         <GalleryDetailSection
           name='Gallery'
@@ -11,7 +11,7 @@ export default function CreateGalleryPage() {
           price={10}
         />
       </div>
-      <div className='w-3/4'></div>
+      <div className='w-2/3'></div>
     </div>
   )
 }
