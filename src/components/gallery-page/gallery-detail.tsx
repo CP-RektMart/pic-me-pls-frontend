4260dcd7e7f2d64e8c7100cc9bb2867e8126acc0
Phumsirii <phumsirisumativit@gmail.com>
2025-02-19 21:51:13 +0700

    feat:image upload button

--- src/components/gallery-page/gallery-detail.tsx
+++ src/components/gallery-page/gallery-detail.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { useState } from 'react'
+import React from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import { ImagePlus } from 'lucide-react'
+import { useDropzone } from 'react-dropzone'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -22,6 +25,9 @@ const gallerySchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters'),
   description: z.string().min(2, 'Description must be at least 2 characters'),
   price: z.number().min(0, 'Price must be at least 0'),
+  image: z
+    .instanceof(File)
+    .refine((file) => file.size !== 0, 'Please upload an image'),
 })
 
 type GalleryFormValues = z.infer<typeof gallerySchema>
@@ -38,6 +44,7 @@ export default function GalleryDetailSection({
   price,
 }: GalleryDetailSectionProps) {
   const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [preview, setPreview] = React.useState<string | ArrayBuffer | null>('')
 
   const form = useForm<GalleryFormValues>({
     resolver: zodResolver(gallerySchema),
@@ -45,6 +52,7 @@ export default function GalleryDetailSection({
       name,
       description,
       price,
+      image: new File([''], 'filename'),
     },
   })
 
@@ -66,6 +74,32 @@ export default function GalleryDetailSection({
     //   toast.success('Your gallery has been successfully updated.')
     // }
   }
+
+  const onDrop = React.useCallback(
+    (acceptedFiles: File[]) => {
+      const reader = new FileReader()
+      try {
+        reader.onload = () => setPreview(reader.result)
+        reader.readAsDataURL(acceptedFiles[0])
+        form.setValue('image', acceptedFiles[0])
+        form.clearErrors('image')
+      } catch (error) {
+        console.error(error)
+        setPreview(null)
+        form.resetField('image')
+      }
+    },
+    [form]
+  )
+
+  const { getRootProps, getInputProps, isDragActive, fileRejections } =
+    useDropzone({
+      onDrop,
+      maxFiles: 1,
+      maxSize: 1000000,
+      accept: { 'image/png': [], 'image/jpg': [], 'image/jpeg': [] },
+    })
+
   return (
     <Container className='my-6'>
       <div className='px-5 py-4'>
@@ -134,6 +168,63 @@ export default function GalleryDetailSection({
                         />
                       </FormControl>
                       <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name='image'
+                  render={() => (
+                    <FormItem className='mx-auto md:w-1/2'>
+                      <FormLabel
+                        className={`${
+                          fileRejections.length !== 0 && 'text-destructive'
+                        }`}
+                      >
+                        <h2 className='text-xl font-semibold tracking-tight'>
+                          Upload your image
+                          <span
+                            className={
+                              form.formState.errors.image ||
+                              fileRejections.length !== 0
+                                ? 'text-destructive'
+                                : 'text-muted-foreground'
+                            }
+                          ></span>
+                        </h2>
+                      </FormLabel>
+                      <FormControl>
+                        <div
+                          {...getRootProps()}
+                          className='mx-auto flex cursor-pointer flex-col items-center justify-center gap-y-2 rounded-lg border border-foreground p-8 shadow-sm shadow-foreground'
+                        >
+                          {preview && (
+                            <img
+                              src={preview as string}
+                              alt='Uploaded image'
+                              className='max-h-[400px] rounded-lg'
+                            />
+                          )}
+                          <ImagePlus
+                            className={`size-40 ${preview ? 'hidden' : 'block'}`}
+                          />
+                          <Input {...getInputProps()} type='file' />
+                          {isDragActive ? (
+                            <p>Drop the image!</p>
+                          ) : (
+                            <p>Click here or drag an image to upload it</p>
+                          )}
+                        </div>
+                      </FormControl>
+                      <FormMessage>
+                        {fileRejections.length !== 0 && (
+                          <p>
+                            Image must be less than 1MB and of type png, jpg, or
+                            jpeg
+                          </p>
+                        )}
+                      </FormMessage>
                     </FormItem>
                   )}
                 />
