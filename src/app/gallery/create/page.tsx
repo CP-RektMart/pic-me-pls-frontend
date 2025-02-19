24286d2dc4d3fe53db8222a73b65ef6b362985d3
Phumsirii <phumsirisumativit@gmail.com>
2025-02-19 21:10:36 +0700

    feat: form field for name

--- src/app/gallery/create/page.tsx
+++ src/app/gallery/create/page.tsx
@@ -4,7 +4,12 @@ export default function CreateGalleryPage() {
   return (
     <div className='w-full'>
       <div className='w-1/4'>
-        <GalleryDetailSection />
+        {/* defaultValues */}
+        <GalleryDetailSection
+          name='Gallery'
+          description='This is my gallery'
+          price={0}
+        />
       </div>
       <div className='w-3/4'></div>
     </div>
--- src/components/gallery-page/gallery-detail.tsx
+++ src/components/gallery-page/gallery-detail.tsx
@@ -1,9 +1,107 @@
-export default function GalleryDetailSection() {
+'use client'
+
+import { useState } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+import Container from '../container'
+
+const profileSchema = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters'),
+  description: z.string().min(2, 'Description must be at least 2 characters'),
+  price: z.number().min(0, 'Price must be at least 0'),
+})
+
+type GalleryFormValues = z.infer<typeof profileSchema>
+
+interface GalleryDetailSectionProps {
+  name: string
+  description: string
+  price: number
+}
+
+export default function GalleryDetailSection({
+  name,
+  description,
+  price,
+}: GalleryDetailSectionProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const form = useForm<GalleryFormValues>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      name,
+      description,
+      price,
+    },
+  })
+
+  const onSubmit = async (data: GalleryFormValues) => {
+    setIsEditing((prevState) => !prevState)
+
+    if (!isEditing) {
+      return
+    }
+
+    //mock data usage
+    console.log(data)
+
+    // const response = await updateGallery(data)
+
+    // if (!response.result) {
+    //   toast.error('An error occurred while updating your profile')
+    // } else {
+    //   toast.success('Your profile has been successfully updated.')
+    // }
+  }
   return (
-    <div className='px-5 py-4'>
-      <div className='space-y-2'>
-        <h1 className='text-xl font-bold'>New Gallery</h1>
+    <Container className='my-6'>
+      <div className='px-5 py-4'>
+        <div className='space-y-2'>
+          <h1 className='text-xl font-bold'>New Gallery</h1>
+          <div>
+            <Form {...form}>
+              <form
+                id='profile-form'
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='flex flex-col gap-8 lg:flex-row'
+              >
+                <FormField
+                  control={form.control}
+                  name='name'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className='text-sm font-medium'>
+                        Name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder='Gallery Name'
+                          disabled={!isEditing}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
