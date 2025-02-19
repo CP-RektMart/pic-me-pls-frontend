5fe912bcded50c25a236ca5a7933318b437c93ab
Phumsirii <phumsirisumativit@gmail.com>
2025-02-19 22:45:13 +0700

    fix: page layout ratio

--- src/app/gallery/create/page.tsx
+++ src/app/gallery/create/page.tsx
@@ -3,7 +3,7 @@ import GalleryDetailSection from '@/components/gallery-page/gallery-detail'
 export default function CreateGalleryPage() {
   return (
     <div className='w-full'>
-      <div className='shadow-right h-full w-1/3 shadow-lg shadow-black/100'>
+      <div className='shadow-right h-full w-1/4 shadow-lg shadow-black/100'>
         {/* defaultValues */}
         <GalleryDetailSection
           name='Gallery'
@@ -11,7 +11,7 @@ export default function CreateGalleryPage() {
           price={10}
         />
       </div>
-      <div className='w-2/3'></div>
+      <div className='w-3/4'></div>
     </div>
   )
 }
