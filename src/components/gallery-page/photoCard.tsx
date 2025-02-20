21af0a455cbc621c8188bc5cbd61238bd1a64fad
Phumsirii <phumsirisumativit@gmail.com>
2025-02-20 19:18:53 +0700

    feat: mock photoCard first draft

--- src/components/gallery-page/photoCard.tsx
+++ src/components/gallery-page/photoCard.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image'
+
 export default function PhotoCard() {
-  return <div className='rounded-2xl'></div>
+  return (
+    <div className='flex flex-col rounded-2xl'>
+      <Image src='/mockPhotoCard.svg' alt='image' className='h-3/4 w-full' />
+      <div className='h-1/4 w-full'></div>
+    </div>
+  )
 }
