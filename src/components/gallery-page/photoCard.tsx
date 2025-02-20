a461ac2623fe8156cd306df3697b555db70cf052
Phumsirii <phumsirisumativit@gmail.com>
2025-02-20 19:40:00 +0700

    feat: photoCard form

--- src/components/gallery-page/photoCard.tsx
+++ src/components/gallery-page/photoCard.tsx
@@ -1,10 +1,98 @@
-import Image from 'next/image'
+'use client'
 
-export default function PhotoCard() {
+import React from 'react'
+import { useState } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import Image from 'next/image'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const photoCardSchema = z.object({
+  description: z.string().min(2, 'Description must be at least 2 characters'),
+})
+
+type photoCardFormValues = z.infer<typeof photoCardSchema>
+
+interface photoCardFormProps {
+  description: string
+}
+
+export default function PhotoCard({ description }: photoCardFormProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const form = useForm<photoCardFormValues>({
+    resolver: zodResolver(photoCardSchema),
+    defaultValues: {
+      description,
+    },
+  })
+
+  const onSubmit = async (data: photoCardFormValues) => {
+    setIsEditing((prevState) => !prevState)
+
+    if (!isEditing) {
+      return
+    }
+
+    //mock data usage
+    console.log(data)
+
+    // const response = await updateGallery(data)
+
+    // if (!response.result) {
+    //   toast.error('An error occurred while updating your gallery')
+    // } else {
+    //   toast.success('Your gallery has been successfully updated.')
+    // }
+  }
+
   return (
-    <div className='flex flex-col rounded-2xl'>
-      <Image src='/mockPhotoCard.svg' alt='image' className='h-3/4 w-full' />
-      <div className='h-1/4 w-full'></div>
+    <div className='flex w-auto flex-col rounded-2xl'>
+      <Image
+        src='/mockPhotoCard.svg'
+        alt='image'
+        className='h-auto w-full'
+        layout='responsive'
+        width={10}
+        height={10}
+      />
+
+      <div className='h-1/4 w-full'>
+        <Form {...form}>
+          <form
+            id='gallery-form'
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='flex h-full flex-col gap-y-4'
+          >
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder='Description (Optional)'
+                      disabled={!isEditing}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
     </div>
   )
 }
