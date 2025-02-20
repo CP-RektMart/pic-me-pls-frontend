a3c0ad9abcc6fe9686001438842941fdd24665fa
Phumsirii <phumsirisumativit@gmail.com>
2025-02-20 19:40:18 +0700

    feat: add mock photoCard props

--- src/app/photographer/packages/create/page.tsx
+++ src/app/photographer/packages/create/page.tsx
@@ -1,4 +1,5 @@
 import GalleryDetailSection from '@/components/gallery-page/gallery-detail'
+import PhotoCard from '@/components/gallery-page/photoCard'
 
 export default function CreateGalleryPage() {
   return (
@@ -11,7 +12,11 @@ export default function CreateGalleryPage() {
           price={10}
         />
       </div>
-      <div className='lg:w-3/4'></div>
+      <div className='lg: space-2 grid-cols-2 lg:w-3/4 lg:grid-cols-4'>
+        {[...Array(4)].map((_, i) => (
+          <PhotoCard key={i} description='My Gallery' />
+        ))}
+      </div>
     </div>
   )
 }
