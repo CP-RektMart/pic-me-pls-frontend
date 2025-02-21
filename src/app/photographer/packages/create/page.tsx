51071759da333a971f1401b4cc812a699d19c374
Phumsirii <phumsirisumativit@gmail.com>
2025-02-21 10:45:09 +0700

    feat: add large window, upload photo

--- src/app/photographer/packages/create/page.tsx
+++ src/app/photographer/packages/create/page.tsx
@@ -1,8 +1,91 @@
+'use client'
+
+import React from 'react'
+import { useState } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import Image from 'next/image'
+import { useDropzone } from 'react-dropzone'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import GalleryDetailSection from '@/components/gallery-page/gallery-detail'
 import PhotoCard from '@/components/gallery-page/photoCard'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const gallerySchema = z.object({
+  image: z
+    .instanceof(File)
+    .refine((file) => file.size !== 0, 'Please upload an image'),
+})
+
+type GalleryFormValues = z.infer<typeof gallerySchema>
 
 export default function CreateGalleryPage() {
-  const galleries = [...Array(8)]
+  //mock gallery data
+  const galleries = [...Array(0)]
+
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [preview, setPreview] = React.useState<string | ArrayBuffer | null>('')
+
+  const form = useForm<GalleryFormValues>({
+    resolver: zodResolver(gallerySchema),
+    defaultValues: {
+      image: new File([''], 'filename'),
+    },
+  })
+
+  const onSubmit = async (data: GalleryFormValues) => {
+    setIsEditing((prevState) => !prevState)
+
+    if (!isEditing) {
+      return
+    }
+
+    //mock data usage
+    console.log(data)
+
+    // const response = await updateGallery(data)
+
+    // if (!response.result) {
+    //   toast.error('An error occurred while updating your gallery')
+    // } else {
+    //   toast.success('Your gallery has been successfully updated.')
+    // }
+  }
+
+  const onDrop = React.useCallback(
+    (acceptedFiles: File[]) => {
+      const reader = new FileReader()
+      try {
+        reader.onload = () => setPreview(reader.result)
+        reader.readAsDataURL(acceptedFiles[0])
+        form.setValue('image', acceptedFiles[0])
+        form.clearErrors('image')
+      } catch (error) {
+        console.error(error)
+        setPreview(null)
+        form.resetField('image')
+      }
+    },
+    [form]
+  )
+
+  const { getRootProps, getInputProps, isDragActive, fileRejections } =
+    useDropzone({
+      onDrop,
+      maxFiles: 1,
+      maxSize: 1000000,
+      accept: { 'image/png': [], 'image/jpg': [], 'image/jpeg': [] },
+    })
+
   return (
     <div className='flex w-full flex-col lg:flex-row'>
       <div className='shadow-right h-full shadow-lg shadow-black/100 lg:w-1/4'>
@@ -16,7 +99,61 @@ export default function CreateGalleryPage() {
       <div className='lg:w-3/4'>
         <div className='grid grid-cols-2 gap-4 p-4 lg:grid-cols-4'>
           {galleries.length === 0 ? (
-            <p>No galleries available</p>
+            <div>
+              <Form {...form}>
+                <form
+                  id='gallery-form'
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className='flex h-full flex-col gap-y-4'
+                >
+                  <FormField
+                    control={form.control}
+                    name='image'
+                    render={() => (
+                      <FormItem>
+                        <FormControl>
+                          <div
+                            {...getRootProps()}
+                            className='flex max-h-10 cursor-pointer flex-row items-center justify-center gap-x-2 rounded-lg border border-foreground bg-zinc-50 py-2 shadow-sm shadow-foreground'
+                          >
+                            {preview && (
+                              <Image
+                                src={preview as string}
+                                alt='Uploaded image'
+                                className='rounded-lg'
+                                width={600} // Adjust width as needed
+                                height={400} // Adjust height as needed
+                                style={{ maxHeight: '400px', width: 'auto' }} // Ensure it respects max height
+                              />
+                            )}
+                            <Image
+                              src={'/uploadPhotoButton.svg'}
+                              alt='icon'
+                              width={24}
+                              height={24}
+                            />
+                            <Input {...getInputProps()} type='file' />
+                            {isDragActive ? (
+                              <p className='text-sm'>Drop the image!</p>
+                            ) : (
+                              <p className='text-sm'>Upload Photos</p>
+                            )}
+                          </div>
+                        </FormControl>
+                        <FormMessage>
+                          {fileRejections.length !== 0 && (
+                            <p>
+                              Image must be less than 1MB and of type png, jpg,
+                              or jpeg
+                            </p>
+                          )}
+                        </FormMessage>
+                      </FormItem>
+                    )}
+                  />
+                </form>
+              </Form>
+            </div>
           ) : (
             galleries.map((_, i) => (
               <div className='flex' key={i}>
