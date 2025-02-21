73547592b34275dbef99544a0dd627ec01661c73
Phumsirii <phumsirisumativit@gmail.com>
2025-02-21 10:35:46 +0700

    fix: redefine gallery explicitly

--- src/app/photographer/packages/create/page.tsx
+++ src/app/photographer/packages/create/page.tsx
@@ -2,6 +2,7 @@ import GalleryDetailSection from '@/components/gallery-page/gallery-detail'
 import PhotoCard from '@/components/gallery-page/photoCard'
 
 export default function CreateGalleryPage() {
+  const galleries = [...Array(8)]
   return (
     <div className='flex w-full flex-col lg:flex-row'>
       <div className='shadow-right h-full shadow-lg shadow-black/100 lg:w-1/4'>
@@ -14,7 +15,7 @@ export default function CreateGalleryPage() {
       </div>
       <div className='lg:w-3/4'>
         <div className='grid grid-cols-2 gap-4 p-4 lg:grid-cols-4'>
-          {[...Array(8)].map((_, i) => (
+          {galleries.map((_, i) => (
             <div className='flex' key={i}>
               <PhotoCard
                 key={i}
