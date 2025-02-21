9a7e20189019eea1ea9bc27d7e7a0c3bad6bf08b
Phumsirii <phumsirisumativit@gmail.com>
2025-02-21 22:55:58 +0700

    fix: remove default values

--- src/components/gallery-page/create-gallery.tsx
+++ src/components/gallery-page/create-gallery.tsx
@@ -90,11 +90,7 @@ export default function CreateGallery() {
     <div className='flex w-full flex-col bg-gray-100 lg:flex-row'>
       <div className='shadow-right space-between h-full bg-white px-5 py-4 shadow-black/100 drop-shadow-lg lg:w-1/4'>
         {/* defaultValues */}
-        <GalleryDetailSection
-          name='Gallery'
-          description='This is my gallery'
-          price={10}
-        />
+        <GalleryDetailSection name='' description='' price={0} />
       </div>
       <div className='lg:w-3/4'>
         {galleries.length === 0 ? (
