dc0853d6b460fc41db7fe71f8bfaf8396b55ddd4
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-02-24 22:50:22 +0700

    feat: quotation details

--- src/components/customer-quotation/details.tsx
+++ src/components/customer-quotation/details.tsx
@@ -1,7 +1,68 @@
-export default function QuotationDetails() {
+import React from 'react'
+
+import { cn } from '@/lib/utils'
+
+import { Separator } from '@/components/ui/separator'
+
+interface QuotationDetailsProps {
+  quotationId: number
+  status: 'Pending' | 'Confirm' | 'Paid' | 'Cancelled'
+  packageName: string
+  photographerName: string
+  customerName: string
+  from: string
+  to: string
+  description: string
+  duration: number
+  totalPrice: number
+}
+
+const statusColor = {
+  Pending: 'text-amber-600',
+  Confirm: 'text-sky-600',
+  Paid: 'text-emerald-600',
+  Cancelled: 'text-red-600',
+}
+
+export default function QuotationDetails({
+  quotationId,
+  status,
+  packageName,
+  photographerName,
+  customerName,
+  from,
+  to,
+  description,
+  duration,
+  totalPrice,
+}: QuotationDetailsProps) {
+  const detailItems = [
+    { title: 'Quotation ID', info: quotationId },
+    { title: 'Status', info: status, style: statusColor[status] },
+    { title: 'Package Name', info: packageName },
+    { title: 'Photographer Name', info: photographerName },
+    { title: 'Customer Name', info: customerName },
+    { title: 'From', info: from },
+    { title: 'To', info: to },
+    { title: 'Description', info: description },
+    { title: 'Duration', info: `${duration} Hour${duration > 1 ? 's' : ''}` },
+    { title: 'Total Price', info: `${totalPrice} Baht` },
+  ]
   return (
-    <div>
-      <h1>details</h1>
+    <div className='mx-auto flex justify-center p-4'>
+      <div className='flex w-full flex-col'>
+        {detailItems.map((item, i) => (
+          <React.Fragment key={i}>
+            <div className='flex flex-row justify-between'>
+              <p className='text-nowrap text-sm text-zinc-700'>{item.title}</p>
+              <p className={cn('w-[200px] text-end text-sm', item.style)}>
+                {item.info}
+              </p>
+            </div>
+            <Separator className='my-3 bg-zinc-200 last:hidden' />
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/customer-quotation/index.tsx
+++ src/components/customer-quotation/index.tsx
@@ -3,9 +3,19 @@ import QuotationDetails from '@/components/customer-quotation/details'
 
 export default async function Page({ id }: { id: number }) {
   return (
-    <div>
-      Quotation id: {id}
-      <QuotationDetails />
+    <div className='w-full p-4'>
+      <QuotationDetails
+        quotationId={id}
+        status='Paid'
+        packageName='Package A'
+        photographerName='Chanatpakorn S.'
+        customerName='Chanotai K.'
+        from='20/02/2025 14:00'
+        to='20/02/2025 19:00'
+        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas augue tortor, luctus eget laoreet et, interdum fringilla urna. '
+        duration={5}
+        totalPrice={2000}
+      />
       <QuotationCarousel />
     </div>
   )
