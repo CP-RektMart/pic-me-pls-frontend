bf064d1008813eaa41e7be81bbf155a798301794
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 15:44:12 +0700

    style: quotation detail page layout

--- src/components/quotation-detail-page/quotation-detail.tsx
+++ src/components/quotation-detail-page/quotation-detail.tsx
@@ -1,3 +1,7 @@
 export default function QuotationDetail() {
-  return <div>Quotation Detail Page Component</div>
+  return (
+    <div className='w-full gap-6 px-4 py-2 lg:px-16 lg:py-4'>
+      <div className='text-2xl font-bold'>Quotation</div>
+    </div>
+  )
 }
