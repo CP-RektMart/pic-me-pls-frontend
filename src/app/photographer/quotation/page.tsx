a433c96230cab2e957f1c9e3b6ff7696965b2c5d
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 22:01:45 +0700

    style: fix style for mobile UI

--- src/app/photographer/quotation/page.tsx
+++ src/app/photographer/quotation/page.tsx
@@ -18,7 +18,7 @@ const quotations: quotation[] = []
 
 export default function QuotationPage() {
   return (
-    <div className='flex w-full flex-col gap-6 px-32 py-4'>
+    <div className='flex w-full flex-col gap-6 px-4 py-4 lg:px-32'>
       <div className='flex flex-row justify-between'>
         <div className='text-2xl font-bold'>Quotation Manager</div>
 
