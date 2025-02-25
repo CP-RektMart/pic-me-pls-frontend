d73d3f4bb1b271ac3687be98f6dfda02cb3ffd43
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 22:00:32 +0700

    style: UI when there are no quotations to show

--- src/components/quotation/photographer-quotation.tsx
+++ src/components/quotation/photographer-quotation.tsx
@@ -1,4 +1,5 @@
 import { quotation } from '@/app/photographer/quotation/page'
+import { Icon } from '@iconify/react'
 
 interface PhotographerQuotationProps {
   quotations: quotation[]
@@ -8,8 +9,13 @@ export default function PhotographerQuotation({
   quotations,
 }: PhotographerQuotationProps) {
   return (
-    <div className='w-full'>
-      {quotations.length == 0 ? <div>No Quotations</div> : null}
+    <div className='size-full'>
+      {quotations.length == 0 ? (
+        <div className='flex h-full flex-col items-center justify-center gap-3'>
+          <Icon icon='lucide:sticky-note' className='size-20' />
+          No Quotations To Show
+        </div>
+      ) : null}
     </div>
   )
 }
