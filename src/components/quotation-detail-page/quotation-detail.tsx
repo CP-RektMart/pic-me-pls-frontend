263b2a6a5260280223841591b3d1991c1b108f51
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 16:53:12 +0700

    style: add padding

--- src/components/quotation-detail-page/quotation-detail.tsx
+++ src/components/quotation-detail-page/quotation-detail.tsx
@@ -3,7 +3,7 @@ import QuotationPackageDetail from './quotation-package-detail'
 
 export default function QuotationDetail() {
   return (
-    <div className='w-full gap-6 px-4 py-2 lg:px-16 lg:py-4'>
+    <div className='w-full gap-6 px-4 py-6 lg:px-16'>
       <div className='text-2xl font-bold'>Quotation</div>
       <div className='grid grid-cols-1 gap-6 lg:grid-cols-2'>
         <QuotationPackageDetail
