c00657f512c0d53b9449673eec5dd25fbe1ad2f7
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 17:22:31 +0700

    feat: add button for 2 states

--- src/components/quotation-detail-page/quotation-detail-section.tsx
+++ src/components/quotation-detail-page/quotation-detail-section.tsx
@@ -1,3 +1,5 @@
+import { Button } from '../ui/button'
+
 interface QuotationDetailSectionProps {
   quotationID: string
   status: string
@@ -40,95 +42,117 @@ export default function QuotationDetailSection({
   }
 
   return (
-    <div className='flex flex-col gap-4 px-5 py-6 lg:px-20'>
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Quotation ID</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {quotationID}
+    <div className='space-y-2.5'>
+      <div className='flex flex-col gap-4 px-5 py-6 lg:px-20'>
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Quotation ID</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {quotationID}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Status</div>
+          <div
+            className={`max-h-24 max-w-48 overflow-hidden text-ellipsis text-right ${statusColour}`}
+          >
+            {status}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Package Name</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {packageName}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Photographer Name</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {photographerName}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Customer Name</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {customerName}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>From</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {from}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>To</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {to}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Description</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {description}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Duration</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {duration}
+          </div>
+        </div>
+
+        <hr className='border border-gray-200' />
+
+        <div className='flex flex-row justify-between text-sm font-normal'>
+          <div>Total Price</div>
+          <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
+            {totalPrice} Baht
+          </div>
         </div>
       </div>
 
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Status</div>
-        <div
-          className={`max-h-24 max-w-48 overflow-hidden text-ellipsis text-right ${statusColour}`}
-        >
-          {status}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Package Name</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {packageName}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Photographer Name</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {photographerName}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Customer Name</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {customerName}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>From</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {from}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>To</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {to}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Description</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {description}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Duration</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {duration}
-        </div>
-      </div>
-
-      <hr className='border border-gray-200' />
-
-      <div className='flex flex-row justify-between text-sm font-normal'>
-        <div>Total Price</div>
-        <div className='max-h-24 max-w-48 overflow-hidden text-ellipsis text-right'>
-          {totalPrice} Baht
-        </div>
+      <div className='px-5 lg:px-20'>
+        {status === 'Pending' && (
+          <div className='flex max-h-10 flex-row gap-2'>
+            <Button className='size-full rounded-md bg-red-700 p-4 text-sm font-medium text-white'>
+              Cancel
+            </Button>
+            <Button className='size-full rounded-md p-4 text-sm font-medium text-white'>
+              Confirm
+            </Button>
+          </div>
+        )}
+        {status === 'Confirmed' && (
+          <div className='flex max-h-10 flex-row gap-2'>
+            <Button className='size-full rounded-md p-4 text-sm font-medium text-white'>
+              Pay
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   )
