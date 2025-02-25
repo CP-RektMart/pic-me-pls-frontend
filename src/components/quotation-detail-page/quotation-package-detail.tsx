806cf842be306cf2c76c374eddfb8e306a73421d
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 16:00:07 +0700

    chore: left side quotation header

--- src/components/quotation-detail-page/quotation-package-detail.tsx
+++ src/components/quotation-detail-page/quotation-package-detail.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function QuotationPackageDetail() {
   return (
     <div className='gap-6 p-6'>
-      <div className='flex flex-row items-center'>
+      <div className='flex flex-row items-center gap-6'>
         <Image
           src='/photographerProfile.svg'
           alt='Photographer'
@@ -11,6 +11,12 @@ export default function QuotationPackageDetail() {
           width={112}
           height={112}
         />
+        <div className='flex flex-col'>
+          <div className='flex flex-row gap-2.5 text-2xl font-bold'>
+            Patthapol Kittikun
+          </div>
+          <div className='text-sm font-medium text-gray-500'>10 Galleries</div>
+        </div>
       </div>
     </div>
   )
