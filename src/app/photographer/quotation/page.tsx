3328741a05e1c0fa0ed3b053e5dbc2e15a83055a
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 21:39:00 +0700

    feat: initialise photographer quotation page

--- src/app/photographer/quotation/page.tsx
+++ src/app/photographer/quotation/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function QuotationPage() {
-  return <div>QuotationPage</div>
+  return (
+    <div className='flex w-full flex-row justify-between px-32 py-4'>
+      <div className='text-2xl font-bold'>Quotation Manager</div>
+
+      <Button>New Quotation</Button>
+    </div>
+  )
 }
---
+++ src/components/quotation/photographer-quotation.tsx
@@ -0,0 +1,3 @@
+export default function PhotographerQuotation() {
+  return <div>PhotographerQuotation</div>
+}
