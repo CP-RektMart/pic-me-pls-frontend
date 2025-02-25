65cc8aa66b6bd7ed581220c7ac018c766afbcb9e
Phumsirii <phumsirisumativit@gmail.com>
2025-02-25 15:45:34 +0700

    style: init detail section grid

--- src/components/quotation-detail-page/quotation-detail.tsx
+++ src/components/quotation-detail-page/quotation-detail.tsx
@@ -2,6 +2,7 @@ export default function QuotationDetail() {
   return (
     <div className='w-full gap-6 px-4 py-2 lg:px-16 lg:py-4'>
       <div className='text-2xl font-bold'>Quotation</div>
+      <div className='grid grid-cols-1 gap-6 lg:grid-cols-2'></div>
     </div>
   )
 }
