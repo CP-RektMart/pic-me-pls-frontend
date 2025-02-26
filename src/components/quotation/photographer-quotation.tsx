1e6477fdbbc216ee5a25190ab595261a2eba7277
Phumsirii <phumsirisumativit@gmail.com>
2025-02-26 11:22:04 +0700

    feat: update logic in showing windows

--- src/components/quotation/photographer-quotation.tsx
+++ src/components/quotation/photographer-quotation.tsx
@@ -74,7 +74,7 @@ export default function PhotographerQuotation({
 
   return (
     <div className='size-full'>
-      {quotations.length == 0 ? (
+      {quotations.length == 0 && !isCreating ? (
         <div className='flex h-full flex-col items-center justify-center gap-3'>
           <Icon icon='lucide:sticky-note' className='size-20' />
           No Quotations To Show
