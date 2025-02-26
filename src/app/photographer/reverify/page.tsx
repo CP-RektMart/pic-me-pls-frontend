a2349400d0e14325da3f55243154c862299265ba
boomchanotai <boom9387@hotmail.com>
2025-02-26 14:57:04 +0700

    refactor: reverify

--- src/app/photographer/reverify/page.tsx
+++ src/app/photographer/reverify/page.tsx
@@ -1,3 +1,4 @@
+import { client } from '@/api/client'
 import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 
@@ -10,27 +11,26 @@ export default async function Page() {
     redirect('/login')
   }
 
-  const response = await fetch(
-    `${process.env.BACKEND_URL}/api/v1/photographer/citizen-card`,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${session.accessToken}`,
-      },
-    }
+  const { data, response } = await client.GET(
+    '/api/v1/photographer/citizen-card'
   )
+
+  console.log(response)
 
   if (!response.ok) {
     redirect('/login')
   }
 
-  const citizenCardInfo = (await response.json()).result
   return (
     <ReverifyPhotographer
-      citizenId={citizenCardInfo.citizenId}
-      laserId={citizenCardInfo.laserId}
-      picture={citizenCardInfo.picture}
-      expireDate={new Date(citizenCardInfo.expireDate)}
+      citizenId={data?.result?.citizenId || ''}
+      laserId={data?.result?.laserId || ''}
+      picture={data?.result?.picture || ''}
+      expireDate={
+        data?.result?.expireDate
+          ? new Date(data?.result?.expireDate)
+          : new Date()
+      }
     />
   )
 }
