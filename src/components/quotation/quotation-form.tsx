e4d2e43cdaca7dffba84c3af245ca27ed698e1a3
Phumsirii <phumsirisumativit@gmail.com>
2025-03-10 12:21:41 +0700

    refactor: create a date time picker component

--- src/components/quotation/quotation-form.tsx
+++ src/components/quotation/quotation-form.tsx
@@ -29,6 +29,7 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 
+import { DateTimePicker } from '../ui/date-time-picker'
 import { Textarea } from '../ui/text-area'
 import {
   CreateQuotationProps,
@@ -148,115 +149,11 @@ export default function QuotationForm({
           render={({ field }) => (
             <FormItem className='flex flex-col'>
               <FormLabel>From</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={'outline'}
-                      className={cn(
-                        'w-full pl-3 text-left font-normal',
-                        !field.value && 'text-muted-foreground'
-                      )}
-                    >
-                      {field.value ? (
-                        new Date(field.value).toLocaleString('en-GB', {
-                          day: '2-digit',
-                          month: '2-digit',
-                          year: 'numeric',
-                          hour: '2-digit',
-                          minute: '2-digit',
-                        })
-                      ) : (
-                        <span>DD/MM/YYYY HH:mm</span>
-                      )}
-                      <Icon
-                        icon='uil:calender'
-                        className='ml-auto h-4 w-4 opacity-50'
-                      />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className='w-auto p-0'>
-                  <div className='lg:flex'>
-                    <Calendar
-                      mode='single'
-                      selected={field.value ? new Date(field.value) : undefined}
-                      onSelect={field.onChange}
-                      initialFocus
-                    />
-                    <div className='flex flex-col divide-y lg:h-[300px] lg:flex-row lg:divide-x lg:divide-y-0'>
-                      <ScrollArea className='w-64 lg:w-auto'>
-                        <div className='flex p-2 lg:flex-col'>
-                          {Array.from({ length: 24 }, (_, i) => i).map(
-                            (hour) => (
-                              <Button
-                                key={hour}
-                                size='icon'
-                                variant={
-                                  field.value &&
-                                  new Date(field.value).getHours() === hour
-                                    ? 'default'
-                                    : 'ghost'
-                                }
-                                className='aspect-square shrink-0 lg:w-full'
-                                onClick={() =>
-                                  field.onChange(
-                                    formatDate(
-                                      new Date(
-                                        new Date(field.value).setHours(hour)
-                                      )
-                                    )
-                                  )
-                                }
-                              >
-                                {hour}
-                              </Button>
-                            )
-                          )}
-                        </div>
-                        <ScrollBar
-                          orientation='horizontal'
-                          className='lg:hidden'
-                        />
-                      </ScrollArea>
-                      <ScrollArea className='w-64 lg:w-auto'>
-                        <div className='flex p-2 lg:flex-col'>
-                          {Array.from({ length: 12 }, (_, i) => i * 5).map(
-                            (minute) => (
-                              <Button
-                                key={minute}
-                                size='icon'
-                                variant={
-                                  field.value &&
-                                  new Date(field.value).getMinutes() === minute
-                                    ? 'default'
-                                    : 'ghost'
-                                }
-                                className='aspect-square shrink-0 lg:w-full'
-                                onClick={() =>
-                                  field.onChange(
-                                    formatDate(
-                                      new Date(
-                                        new Date(field.value).setMinutes(minute)
-                                      )
-                                    )
-                                  )
-                                }
-                              >
-                                {minute.toString().padStart(2, '0')}
-                              </Button>
-                            )
-                          )}
-                        </div>
-                        <ScrollBar
-                          orientation='horizontal'
-                          className='lg:hidden'
-                        />
-                      </ScrollArea>
-                    </div>
-                  </div>
-                </PopoverContent>
-              </Popover>
+              <DateTimePicker
+                value={field.value}
+                onChange={field.onChange}
+                formatDate={formatDate}
+              />
               <FormMessage />
             </FormItem>
           )}
