a09e00133a68014bc4d9069f7edfcbaf08dc28fc
Phumsirii <phumsirisumativit@gmail.com>
2025-03-10 22:51:29 +0700

    fix: use text area for packages create page

--- src/components/photographer/package-page/package-detail.tsx
+++ src/components/photographer/package-page/package-detail.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/text-area'
 
 import { CreatePackageForm, CreatePhotoCardForm } from './create-package'
 
@@ -72,7 +73,11 @@ export default function PackageDetailSection({
           <FormItem>
             <FormLabel className='text-sm font-medium'>Description</FormLabel>
             <FormControl>
-              <Input placeholder='Description' {...field} />
+              <Textarea
+                placeholder='Description'
+                {...field}
+                className='font-normal'
+              />
             </FormControl>
             <FormMessage />
           </FormItem>
