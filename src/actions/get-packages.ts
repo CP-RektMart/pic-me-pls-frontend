7a85c4b0f9b0facf260907eda2b35e7a19414617
Phumsirii <phumsirisumativit@gmail.com>
2025-03-11 12:37:47 +0700

    refacotr: move packages to src/action

---
+++ src/actions/get-packages.ts
@@ -0,0 +1,19 @@
+import { EditPackageForm } from '@/components/photographer/package-page/edit-package'
+
+export const packages: EditPackageForm[] = [
+  {
+    name: 'Wedding Package',
+    packageDescription: 'Wedding photography package',
+    price: 400,
+  },
+  {
+    name: 'Birthday Package',
+    packageDescription: 'Birthday photography package',
+    price: 300,
+  },
+  {
+    name: 'Graduation Package',
+    packageDescription: 'Graduation photography package',
+    price: 200,
+  },
+]
--- src/app/photographer/quotation/page.tsx
+++ src/app/photographer/quotation/page.tsx
@@ -1,25 +1,7 @@
+import { packages } from '@/actions/get-packages'
 import { quotations } from '@/actions/get-quotations'
 
-import { EditPackageForm } from '@/components/photographer/package-page/edit-package'
 import PhotographerQuotation from '@/components/quotation/photographer-quotation'
-
-const packages: EditPackageForm[] = [
-  {
-    name: 'Wedding Package',
-    packageDescription: 'Wedding photography package',
-    price: 400,
-  },
-  {
-    name: 'Birthday Package',
-    packageDescription: 'Birthday photography package',
-    price: 300,
-  },
-  {
-    name: 'Graduation Package',
-    packageDescription: 'Graduation photography package',
-    price: 200,
-  },
-]
 
 export default function QuotationPage() {
   return (
