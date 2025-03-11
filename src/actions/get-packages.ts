e4df7fb612f14900870534cb0bb31653095a610c
Phumsirii <phumsirisumativit@gmail.com>
2025-03-11 15:47:24 +0700

    fix: mock getQuotations and packages function

--- src/actions/get-packages.ts
+++ src/actions/get-packages.ts
@@ -1,19 +1,21 @@
 import { EditPackageForm } from '@/components/photographer/package-page/edit-package'
 
-export const packages: EditPackageForm[] = [
-  {
-    name: 'Wedding Package',
-    packageDescription: 'Wedding photography package',
-    price: 400,
-  },
-  {
-    name: 'Birthday Package',
-    packageDescription: 'Birthday photography package',
-    price: 300,
-  },
-  {
-    name: 'Graduation Package',
-    packageDescription: 'Graduation photography package',
-    price: 200,
-  },
-]
+export function getPackages(): EditPackageForm[] {
+  return [
+    {
+      name: 'Wedding Package',
+      packageDescription: 'Wedding photography package',
+      price: 400,
+    },
+    {
+      name: 'Birthday Package',
+      packageDescription: 'Birthday photography package',
+      price: 300,
+    },
+    {
+      name: 'Graduation Package',
+      packageDescription: 'Graduation photography package',
+      price: 200,
+    },
+  ]
+}
--- src/app/photographer/quotation/page.tsx
+++ src/app/photographer/quotation/page.tsx
@@ -1,8 +1,11 @@
-import { packages } from '@/actions/get-packages'
-import { quotations } from '@/actions/get-quotations'
+import { getPackages } from '@/actions/get-packages'
+import { getQuotations } from '@/actions/get-quotations'
 
 import PhotographerQuotation from '@/components/quotation/photographer-quotation'
 
 export default function QuotationPage() {
+  const quotations = getQuotations()
+  const packages = getPackages()
+
   return <PhotographerQuotation quotations={quotations} packages={packages} />
 }
