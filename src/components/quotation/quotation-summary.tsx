9c4d059b2af26d5aab084079fe26c3e75df6631e
Phumsirii <phumsirisumativit@gmail.com>
2025-03-11 14:03:40 +0700

    feat: initialise quotation summary component

---
+++ src/components/quotation/quotation-summary.tsx
@@ -0,0 +1,40 @@
+import { EditPackageForm } from '../photographer/package-page/edit-package'
+
+interface QuotationSummaryProps {
+  totalHours: number
+  selectedPackage: string
+  packages: EditPackageForm[]
+}
+
+export default function QuotationSummary({
+  packages,
+  totalHours,
+  selectedPackage,
+}: QuotationSummaryProps) {
+  return (
+    <div className='flex flex-col gap-2 p-4 text-sm font-normal'>
+      <div className='flex flex-row justify-between'>
+        <div>Total hours</div>
+        <div>{totalHours > 0 ? totalHours.toFixed(0) : 0} Hours</div>
+      </div>
+
+      <div className='flex flex-row justify-between'>
+        <div>Price Per Hour</div>
+        <div>
+          {packages.find((pkg) => pkg.name === selectedPackage)?.price ?? 0}{' '}
+          Baht
+        </div>
+      </div>
+
+      <div className='flex flex-row justify-between text-base font-bold'>
+        <div>Total Price</div>
+        <div>
+          {((packages.find((pkg) => pkg.name === selectedPackage)?.price ??
+            0) ||
+            0) * totalHours}{' '}
+          Baht
+        </div>
+      </div>
+    </div>
+  )
+}
