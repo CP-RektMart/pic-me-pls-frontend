6400ca74473eaa2961241a9b49e7f9bda2491eb2
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2025-03-12 20:26:09 +0700

    chore:popover z-index

--- src/components/home-page/filter-popover.tsx
+++ src/components/home-page/filter-popover.tsx
@@ -21,8 +21,8 @@ export default function FilterPopover({
   setSelectedCategories: (value: number[]) => void
 }) {
   return (
-    <div className='absolute -right-16 top-10 flex justify-center md:-right-12'>
-      <div className='flex w-80 flex-col items-center justify-center gap-4 rounded-lg border border-gray-300 p-4 shadow-md'>
+    <div className='absolute -right-16 top-10 z-50 flex justify-center md:-right-12'>
+      <div className='flex w-80 flex-col items-center justify-center gap-4 rounded-lg border border-gray-300 bg-white p-4 shadow-md'>
         <FilterPrice
           minPrice={minPrice}
           setMinPrice={setMinPrice}
