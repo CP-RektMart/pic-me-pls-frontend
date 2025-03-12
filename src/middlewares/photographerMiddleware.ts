301361e3d02e76ad838fbcda9b5fdb63e793a492
boomchanotai <boom9387@hotmail.com>
2025-03-12 16:08:50 +0700

    fix: middleware

---
+++ src/middlewares/photographerMiddleware.ts
@@ -0,0 +1,25 @@
+import { auth } from '@/auth'
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+
+import { MiddlewareFactory } from './chain'
+
+export const photographerMiddleware: MiddlewareFactory = (next) => {
+  return async (request: NextRequest, event: NextFetchEvent) => {
+    const session = await auth()
+    const user = session?.user
+
+    const isPhotographerPath =
+      request.nextUrl.pathname.startsWith('/photographer')
+
+    if (isPhotographerPath) {
+      if (!user) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+      }
+      if (user?.role !== 'PHOTOGRAPHER') {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+      }
+    }
+
+    return next(request, event)
+  }
+}
---
+++ src/middlewares/userMiddleware.ts
@@ -0,0 +1,39 @@
+import { auth } from '@/auth'
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+
+import { MiddlewareFactory } from './chain'
+
+export const userMiddleware: MiddlewareFactory = (next) => {
+  return async (request: NextRequest, event: NextFetchEvent) => {
+    const session = await auth()
+    const user = session?.user
+
+    const isPhotographerPath =
+      request.nextUrl.pathname.startsWith('/photographer')
+
+    if (
+      !isPhotographerPath &&
+      request.nextUrl.pathname !== '/login' &&
+      request.nextUrl.pathname !== '/sign-up' &&
+      request.nextUrl.pathname !== '/profile'
+    ) {
+      if (!user) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+      }
+      if (user?.role === 'PHOTOGRAPHER') {
+        return NextResponse.redirect(new URL('/photographer', request.nextUrl))
+      }
+    }
+
+    if (
+      request.nextUrl.pathname === '/login' ||
+      request.nextUrl.pathname === '/sign-up'
+    ) {
+      if (user) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+      }
+    }
+
+    return next(request, event)
+  }
+}
