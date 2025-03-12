c71f9bddf6b377fb144635918bd725809b06ea25
boomchanotai <boom9387@hotmail.com>
2025-03-12 14:09:48 +0700

    fix: route

--- src/data/nav-items.ts
+++ src/data/nav-items.ts
@@ -1,8 +1,11 @@
 export const photographerItems = [
-  { icon: 'lucide:home', title: 'Home', url: '/' },
-  { icon: 'lucide:file-chart-column', title: 'Quotation', url: '/quotation' },
-  { icon: 'lucide:package', title: 'Package', url: '/package' },
-  { icon: 'lucide:message-circle', title: 'Review', url: '/review' },
+  { icon: 'lucide:home', title: 'Home', url: '/photographer' },
+  {
+    icon: 'lucide:file-chart-column',
+    title: 'Quotation',
+    url: '/photographer/quotation',
+  },
+  { icon: 'lucide:package', title: 'Package', url: '/photographer/package' },
   { icon: 'lucide:user', title: 'Profile', url: '/profile' },
 ]
 
