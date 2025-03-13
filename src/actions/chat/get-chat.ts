0473047651b3a8e3dd4c066ed072ae09f0406abd
Phumsirii <phumsirisumativit@gmail.com>
2025-03-13 21:51:11 +0700

    chore: add mock getChats function

---
+++ src/actions/chat/get-chat.ts
@@ -0,0 +1,55 @@
+export interface Chat {
+  id: string
+  description: string
+  conversation: TextMessage[]
+}
+
+export interface TextMessage {
+  id: string
+  message: string
+  chatId: string
+  sender: 'photographer' | 'customer'
+}
+
+export const mockChats: Chat[] = [
+  {
+    id: '1',
+    description: 'Chat 1',
+    conversation: [
+      {
+        id: '1',
+        message: 'Hello Chat 1',
+        chatId: '1',
+        sender: 'photographer',
+      },
+      {
+        id: '2',
+        message: 'Hi',
+        chatId: '1',
+        sender: 'customer',
+      },
+    ],
+  },
+  {
+    id: '2',
+    description: 'Chat 2',
+    conversation: [
+      {
+        id: '1',
+        message: 'Hello Chat 2',
+        chatId: '2',
+        sender: 'photographer',
+      },
+      {
+        id: '2',
+        message: 'Hi',
+        chatId: '2',
+        sender: 'customer',
+      },
+    ],
+  },
+]
+
+export function getChats(): Chat[] {
+  return mockChats
+}
