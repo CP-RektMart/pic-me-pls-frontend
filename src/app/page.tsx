05b2646d42231b45dbf2ca34c50b131ae327474c
boomchanotai <boom9387@hotmail.com>
2025-03-13 07:22:26 +0700

    fix: type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { getCategories } from '@/actions/get-categories'
 import { getQueryPackages } from '@/actions/package/get-query-packages'
 import { client } from '@/api/client'
-import { Package } from '@/types/package'
+import { PackageVerbose } from '@/types/package'
 import ProfileMockImage from '@public/images/profile-mock-image.png'
 
 import HomePageComponent from '@/components/home-page'
@@ -22,14 +22,16 @@ export default async function Home() {
     pageSize: 10,
   })
   const packagesData = packagesResponse?.data ?? []
-  const packageProps: PackageProps[] = packagesData.map((pkg: Package) => ({
-    title: pkg.name ?? 'Unknown title',
-    photographer: pkg.photographer?.name ?? 'Annonymous',
-    photographerId: Number(pkg.photographer?.id) ?? 'Unknown photographer',
-    category: pkg.category?.name ?? 'Unknown category',
-    price: pkg.price ? `${pkg.price}` : 'Price not available',
-    imageUrl: pkg.media?.[0]?.pictureUrl ?? ProfileMockImage.src,
-  }))
+  const packageProps: PackageProps[] = packagesData.map(
+    (pkg: PackageVerbose) => ({
+      title: pkg.name ?? 'Unknown title',
+      photographer: pkg.photographer?.name ?? 'Annonymous',
+      photographerId: Number(pkg.photographer?.id) ?? 'Unknown photographer',
+      category: pkg.category?.name ?? 'Unknown category',
+      price: pkg.price ? `${pkg.price}` : 'Price not available',
+      imageUrl: pkg.media?.[0]?.pictureUrl ?? ProfileMockImage.src,
+    })
+  )
   const sortedPackages = [...packageProps].sort(
     (a, b) => Number(a.price) - Number(b.price)
   )
