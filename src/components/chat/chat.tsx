a0b224a279b7fb90f2c3f7f94cf6418c7999063e
Phumsirii <phumsirisumativit@gmail.com>
2025-03-18 00:27:45 +0700

    feat: msg sending with send button

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -55,7 +55,7 @@ export default function ChatTab({
         })}
       </div>
 
-      <ChatInputBar />
+      <ChatInputBar currentChat={chat} userRole={userRole} />
     </div>
   )
 }
--- src/components/chat/input-bar.tsx
+++ src/components/chat/input-bar.tsx
@@ -2,17 +2,27 @@
 
 import { useRef } from 'react'
 
+import { Chat } from '@/actions/chat/get-chat'
+import { postChat } from '@/actions/chat/post-chat'
+
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 
-export default function ChatInputBar() {
+export default function ChatInputBar({
+  currentChat,
+  userRole,
+}: {
+  currentChat: Chat | null
+  userRole: 'photographer' | 'customer'
+}) {
   const inputRef = useRef<HTMLInputElement>(null)
 
   const handleClick = () => {
+    if (!currentChat) return
+
     if (inputRef.current) {
       console.log(inputRef.current.value)
-    }
-    if (inputRef.current) {
+      postChat(currentChat.id, inputRef.current.value, userRole)
       inputRef.current.value = ''
     }
   }
