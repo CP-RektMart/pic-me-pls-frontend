ee3a7deed8a1b4f971776b41cd2208b7d66d51cf
Phumsirii <phumsirisumativit@gmail.com>
2025-03-18 00:18:55 +0700

    feat: add postChat method

---
+++ src/actions/chat/post-chat.ts
@@ -0,0 +1,17 @@
+import { mockChats } from './get-chat'
+
+export function postChat(
+  chatId: string,
+  message: string,
+  sender: 'photographer' | 'customer'
+) {
+  const chat = mockChats.find((chat) => chat.id === chatId)
+  if (chat) {
+    chat.conversation.push({
+      id: String(chat.conversation.length + 1),
+      message,
+      chatId,
+      sender,
+    })
+  }
+}
