1bc474712400e61716cc286c0aab2a562b48edf0
Phumsirii <phumsirisumativit@gmail.com>
2025-03-18 23:34:53 +0700

    feat: add quotationID props

--- src/components/chat/quotation-message-card.tsx
+++ src/components/chat/quotation-message-card.tsx
@@ -7,10 +7,11 @@ interface QuotationMessageProps {
 
 export default function QuotationMessage({ message }: QuotationMessageProps) {
   return (
-    <div className='flex h-64 w-72 flex-col p-4'>
+    <div className='flex h-64 w-72 flex-col space-y-3 p-4'>
       <div className='flex flex-row text-base font-bold'>
         {message.messageType}
       </div>
+      <div className='text-xl font-bold'>{message.quotation.quotationID}</div>
     </div>
   )
 }
