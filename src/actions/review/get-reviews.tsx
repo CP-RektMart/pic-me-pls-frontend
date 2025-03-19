bfc12905e323f17ddbf161ea96b5911df3972de6
Phumsirii <phumsirisumativit@gmail.com>
2025-03-19 22:07:16 +0700

    chore: add mock review data

---
+++ src/actions/review/get-reviews.tsx
@@ -0,0 +1,51 @@
+import ProfileMockImage from '@public/images/profile-mock-image.png'
+
+interface Review {
+  id: number
+  reviewer: string
+  comment: string
+  rating: number
+  reviewerProfilePic: string
+}
+
+const mockReviews: Review[] = [
+  {
+    id: 1,
+    reviewer: 'John Doe',
+    comment: 'Great product, I love it!',
+    rating: 5,
+    reviewerProfilePic: ProfileMockImage.src,
+  },
+  {
+    id: 2,
+    reviewer: 'Jane Doe',
+    comment: 'I like it, but it could be better',
+    rating: 4,
+    reviewerProfilePic: ProfileMockImage.src,
+  },
+  {
+    id: 3,
+    reviewer: 'Jack Doe',
+    comment: 'Great product, I love it!',
+    rating: 5,
+    reviewerProfilePic: ProfileMockImage.src,
+  },
+  {
+    id: 4,
+    reviewer: 'James Doe',
+    comment: 'I like it, but it could be better',
+    rating: 4,
+    reviewerProfilePic: ProfileMockImage.src,
+  },
+  {
+    id: 5,
+    reviewer: 'Joe Doe',
+    comment: 'Great product, I love it!',
+    rating: 5,
+    reviewerProfilePic: ProfileMockImage.src,
+  },
+]
+
+export function getReviews(): Review[] {
+  return mockReviews
+}
