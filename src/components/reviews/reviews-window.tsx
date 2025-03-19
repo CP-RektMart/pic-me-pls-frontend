3f6ca524978478c2f782daad80aaed8ae35a801a
Phumsirii <phumsirisumativit@gmail.com>
2025-03-19 22:08:37 +0700

    feat: start mocking data

--- src/components/reviews/reviews-window.tsx
+++ src/components/reviews/reviews-window.tsx
@@ -1,7 +1,29 @@
+import { getReviews } from '@/actions/review/get-reviews'
+import Image from 'next/image'
+
 export default function ReviewsWindow() {
+  const reviews = getReviews()
+
   return (
     <div className='flex flex-col rounded-xl border border-zinc-200 p-6 shadow'>
       <div className='w-full font-semibold'>Reviews</div>
+      <div className='mt-4 flex flex-col'>
+        {reviews.map((review) => (
+          <div key={review.id} className='flex items-center space-x-4'>
+            <Image
+              src={review.reviewerProfilePic}
+              alt='Reviewer Profile'
+              className='h-10 w-10 rounded-full'
+              width={32}
+              height={32}
+            />
+            <div className='flex flex-col'>
+              <div className='font-semibold'>{review.reviewer}</div>
+              <div className='text-sm'>{review.comment}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
