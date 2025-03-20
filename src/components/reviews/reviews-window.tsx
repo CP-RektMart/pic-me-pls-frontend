309b829c3aaab86ce66e2e05db705a691e8fa9d1
Phumsirii <phumsirisumativit@gmail.com>
2025-03-20 15:46:21 +0700

    styling: add placeholder when there are no reviews

--- src/components/reviews/reviews-window.tsx
+++ src/components/reviews/reviews-window.tsx
@@ -1,4 +1,5 @@
 import { getReviews } from '@/actions/review/get-reviews'
+import { Icon } from '@iconify/react'
 
 import ReviewCard from './review-card'
 
@@ -9,11 +10,18 @@ export default function ReviewsWindow() {
     <div className='flex w-full flex-col space-y-4 border-zinc-200 px-6 lg:w-[448px] lg:rounded-xl lg:border lg:p-6 lg:shadow'>
       <div className='w-full font-semibold'>Reviews</div>
       <div className='mt-4 flex flex-col space-y-4'>
-        {reviews.map((review) => (
-          <div key={review.id} className='flex w-full items-center space-x-4'>
-            <ReviewCard review={review} />
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <div key={review.id} className='flex w-full items-center space-x-4'>
+              <ReviewCard review={review} />
+            </div>
+          ))
+        ) : (
+          <div className='flex min-h-96 flex-col items-center justify-center space-y-2'>
+            <Icon icon='material-symbols:search' className='size-12' />
+            No reviews yet
           </div>
-        ))}
+        )}
       </div>
     </div>
   )
