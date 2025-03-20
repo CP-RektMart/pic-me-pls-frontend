eb4cac0ebcc99491392794d59f331fcf3435b28b
tin2003tin <tinsiriwid@gmail.com>
2025-03-21 01:03:25 +0700

    adjust offset

--- src/components/package/goBackButton.tsx
+++ src/components/package/goBackButton.tsx
@@ -8,7 +8,7 @@ const GoBackButton: React.FC<GoBackButtonProps> = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className='absolute left-4 top-[70px] z-10 mb-4 inline-flex items-center rounded-lg bg-black bg-opacity-10 px-4 py-2 text-sm font-bold text-white hover:bg-opacity-20 focus:outline-none'
+      className='absolute left-4 top-[80px] z-10 mb-4 inline-flex items-center rounded-lg bg-black bg-opacity-10 px-4 py-2 text-sm font-bold text-white hover:bg-opacity-20 focus:outline-none md:top-[70px]'
     >
       <ArrowLeft className='mr-2' /> Go Back
     </button>
