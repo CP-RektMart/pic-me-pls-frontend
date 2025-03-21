a8d70ef0e246963bc660ce0e2e6b6838ca935505
Phumsirii <phumsirisumativit@gmail.com>
2025-03-21 22:39:29 +0700

    styling: remove excess styling of dialog footer

--- src/components/reviews/review-window-dialog.tsx
+++ src/components/reviews/review-window-dialog.tsx
@@ -29,7 +29,7 @@ export default function ReviewWindowDialog() {
             <DialogDescription></DialogDescription>
           </DialogHeader>
           <ReviewsWindow />
-          <DialogFooter className='sm:justify-start'></DialogFooter>
+          <DialogFooter></DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
