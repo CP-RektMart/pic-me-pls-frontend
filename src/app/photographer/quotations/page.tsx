424fb168d39ba16258c3a00c247b3748430160aa
tin2003tin <tinsiriwid@gmail.com>
2025-03-26 01:08:04 +0700

    fix: use promise

--- src/app/photographer/quotations/page.tsx
+++ src/app/photographer/quotations/page.tsx
@@ -3,19 +3,23 @@ import { getQuotations } from '@/actions/quotation/get-quotations'
 
 import PhotographerQuotation from '@/components/photographer/quotations/photographer-quotation'
 
+interface SearchParams {
+  id?: string
+  create?: string
+}
+
 export default async function QuotationPage({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | undefined }
+  searchParams?: Promise<SearchParams>
 }) {
   const param = await searchParams
+
   const quotations = await getQuotations()
   const packages = await getPackages()
 
-  const customerId = param?.id ? Number(param.id) : undefined
-
-  const createParam = param?.create
-  const defaultWindow = createParam === '1' ? 'create' : null
+  const customerId = param?.id ? parseInt(param.id, 10) || undefined : undefined
+  const defaultWindow = param?.create === '1' ? 'create' : null
 
   return (
     <PhotographerQuotation
