d238f7c056ff4dd015fa16fe4add854703d7e240
teegoood <teegooodgood@gmail.com>
2025-03-27 05:58:37 +0700

    fix: return pol code

--- src/components/chat/chat-page.tsx
+++ src/components/chat/chat-page.tsx
@@ -2,8 +2,10 @@
 
 import { useEffect, useState } from 'react'
 
+import { getPhotographer } from '@/actions/photographers/get-photographer'
 import { Chat, Message } from '@/types/messages'
 import { User, UserRole } from '@/types/user'
+import { useSearchParams } from 'next/navigation'
 import useWebSocket from 'react-use-websocket'
 
 import ChatList from '@/components/chat/chat-list'
@@ -23,6 +25,8 @@ export default function ChatPage({
   user,
   userId,
 }: Props) {
+  const searchParams = useSearchParams()
+  const photographerId = parseInt(searchParams.get('photographerId') || '0')
   const [chats, setChats] = useState(messages)
   const [selectedChat, setSelectedChat] = useState<Chat | null>(null)
 
@@ -34,6 +38,44 @@ export default function ChatPage({
       onClose: () => console.log('close'),
     }
   )
+
+  useEffect(() => {
+    const fetchAndSetChat = async () => {
+      if (photographerId) {
+        const existingChat = chats.find(
+          (chat) => chat.user.id === photographerId
+        )
+        if (existingChat) {
+          // If an existing chat with photographerId is found, set it as selected
+          setSelectedChat(existingChat)
+        } else {
+          // If no existing chat with photographerId, create new chat
+          try {
+            const photographer = await getPhotographer(photographerId)
+            if (photographer) {
+              const newChat: Chat = {
+                user: {
+                  id: photographerId,
+                  name: photographer.name || '',
+                  profilePictureUrl: photographer.profilePictureUrl || '',
+                  role: 'PHOTOGRAPHER',
+                },
+                messages: [],
+              }
+              setSelectedChat(newChat)
+              setChats((prevChats) => [...prevChats, newChat])
+            }
+          } catch (error) {
+            console.error('Failed to fetch photographer:', error)
+          }
+        }
+      } else {
+        // If no photographerId, set selectedChat to null
+        setSelectedChat(null)
+      }
+    }
+    fetchAndSetChat()
+  }, [photographerId])
 
   const splitFirstSpace = (text: string): string[] => {
     const parts = text.split(' ')
